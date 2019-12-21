ac5fe64f393ff9c21efe270c047790b6f42ce831
Nick Ardecky <nardecky@zingsoft.com>
2019-12-20 16:37:33 -0800

    Finalize base tests

--- test/base-test.spec.js
+++ test/base-test.spec.js
@@ -38,4 +38,31 @@ describe(`Server Route Test's`, function() {
     });
   });
 
+  describe('Get Base Routes', function() {
+    it(' Post / Url is OK', function(done) {
+      axios.post('http://localhost:8080/')
+        .then(function (response) {
+          // console.log(response);
+          expect(response.statusText).to.equal('OK');
+          expect(response.status).to.equal(200);
+          done();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+
+    it('Post /json Url is OK', function(done) {
+      axios.post('http://localhost:8080/json')
+        .then(function (response) {
+          // console.log(response);
+          expect(response.statusText).to.equal('OK');
+          expect(response.status).to.equal(200);
+          done();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+  });
 });
